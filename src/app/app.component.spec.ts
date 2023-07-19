2dee25e2fda58c351ad72b81093802ecd4fcdccc
csherf <chadnsherf@gmail.com>
2023-07-18 19:29:50 -0700

    Built the angular project and deleted the other one

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'csherf-website'`, () => {
+  it(`should have as title 'chads-website'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('csherf-website');
+    expect(app.title).toEqual('chads-website');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('csherf-website app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('chads-website app is running!');
   });
 });
